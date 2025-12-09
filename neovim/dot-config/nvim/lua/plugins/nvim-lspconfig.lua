605d51ee93dffd4322c79ddc1b19161ff9c19aeb
Sindre Moen <sindrenm@gmail.com>
2025-12-09 22:46:35 +0100

    Neovim: Configure QML LSP

--- neovim/dot-config/nvim/lua/plugins/nvim-lspconfig.lua
+++ neovim/dot-config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,7 +5,14 @@ vim.lsp.enable({
   "kotlin_lsp",
   "lua_ls",
   "nil_ls",
+  "qmlls",
   "rust_analyzer",
   "taplo",
   "yamlls",
+})
+
+-- The QML LSP command on Arch is `qmlls6`, not `qmlls`,
+-- which nvim-lspconfig assumes by default
+vim.lsp.config("qmlls", {
+  cmd = { "qmlls6" },
 })
