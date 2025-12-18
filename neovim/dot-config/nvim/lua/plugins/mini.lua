8a37b8686d6c5365eb47b9a72855ba69a84dd1af
Sindre Moen <sindrenm@gmail.com>
2025-12-18 03:40:35 +0100

    Neovim: Remap mini.surround mappings

--- neovim/dot-config/nvim/lua/plugins/mini.lua
+++ neovim/dot-config/nvim/lua/plugins/mini.lua
@@ -66,4 +66,14 @@ require('mini.move').setup({
 require('mini.operators').setup()
 require('mini.pick').setup()
 require('mini.splitjoin').setup()
-require('mini.surround').setup()
+
+require('mini.surround').setup({
+  mappings = {
+    add = '<leader>sa',       -- Add surrounding in Normal and Visual modes
+    delete = '<leader>sd',    -- Delete surrounding
+    find = '<leader>sf',      -- Find surrounding (to the right)
+    find_left = '<leader>sF', -- Find surrounding (to the left)
+    highlight = '<leader>sh', -- Highlight surrounding
+    replace = '<leader>sr',   -- Replace surrounding
+  },
+})
