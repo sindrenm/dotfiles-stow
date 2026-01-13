ad5f3e15243cbc8c6b6b5b644ac85a8e7782e295
Sindre Moen <sindrenm@gmail.com>
2026-01-13 16:40:00 +0100

    Neovim: Order keymaps alphanumerically

--- neovim/dot-config/nvim/lua/config/keymaps.lua
+++ neovim/dot-config/nvim/lua/config/keymaps.lua
@@ -4,14 +4,14 @@ end
 
 vim.keymap.set("n", "-", toggle_mini_files)
 vim.keymap.set("n", "<leader>do", MiniDiff.toggle_overlay)
-vim.keymap.set("n", "<leader>mm", MiniMap.toggle)
 vim.keymap.set("n", "<leader>ds", vim.diagnostic.open_float)
-vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>f", "<CMD>Pick files<CR>")
-vim.keymap.set("n", "<leader>h", "<CMD>Pick help<CR>")
 vim.keymap.set("n", "<leader>g", "<CMD>Pick grep_live<CR>")
-vim.keymap.set("n", "<leader>pi", "<CMD>PasteImage<CR>")
+vim.keymap.set("n", "<leader>h", "<CMD>Pick help<CR>")
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>mm", MiniMap.toggle)
 vim.keymap.set("n", "<leader>o", "<CMD>Outline<CR>")
+vim.keymap.set("n", "<leader>pi", "<CMD>PasteImage<CR>")
 
 local flash = require("flash")
 vim.keymap.set({ "n", "x", "o" }, "<leader>/", flash.jump, { desc = "Flash Jump" })
