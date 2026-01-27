3469fdf5cbc4c8f01cac52f86e5fc9833425db2c
Sindre Moen <sindrenm@gmail.com>
2026-01-27 23:56:58 +0100

    Neovim: Change shortcut for LSP diagnostics

--- neovim/dot-config/nvim/lua/config/keymaps.lua
+++ neovim/dot-config/nvim/lua/config/keymaps.lua
@@ -4,10 +4,10 @@ end
 
 vim.keymap.set("n", "-", toggle_mini_files)
 vim.keymap.set("n", "<leader>do", MiniDiff.toggle_overlay)
-vim.keymap.set("n", "<leader>ds", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>f", "<CMD>Pick files<CR>")
 vim.keymap.set("n", "<leader>g", "<CMD>Pick grep_live<CR>")
 vim.keymap.set("n", "<leader>h", "<CMD>Pick help<CR>")
+vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>mm", MiniMap.toggle)
 vim.keymap.set("n", "<leader>o", "<CMD>Outline<CR>")
