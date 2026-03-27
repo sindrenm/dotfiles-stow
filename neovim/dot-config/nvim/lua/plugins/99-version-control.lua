1c873ec235eeb1396767641aee3d8cd59cecaed3
Sindre Moen <sindrenm@gmail.com>
2026-03-27 11:18:36 +0100

    Neovim: Configure hunk.nvim keybindings

--- neovim/dot-config/nvim/lua/plugins/99-version-control.lua
+++ neovim/dot-config/nvim/lua/plugins/99-version-control.lua
@@ -1,7 +1,38 @@
 local hunk = require("hunk")
 local jj = require("jj")
 
-hunk.setup()
+hunk.setup(
+  {
+    keys = {
+      global = {
+        quit = { "q" },
+        accept = { "<S-Cr>" },
+        focus_tree = { "<leader>e" },
+      },
+
+      tree = {
+        expand_node = { "l", "<Right>" },
+        collapse_node = { "h", "<Left>" },
+
+        open_file = { "<Cr>" },
+
+        toggle_file = { "<Space>" },
+      },
+
+      diff = {
+        toggle_line = { "a" },
+        toggle_line_pair = { "<C-a>" },
+        toggle_hunk = { "<S-a>" },
+
+        prev_hunk = { "[[" },
+        next_hunk = { "]]" },
+
+        -- Jump between the left and right diff view
+        toggle_focus = { "<Tab>" },
+      },
+    },
+  }
+)
 
 jj.setup({
   diff = {
