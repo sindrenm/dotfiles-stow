a6738c64009ec57587af95dc0c63b41834667929
Sindre Moen <sindrenm@gmail.com>
2026-04-09 01:19:57 +0200

    Neovim: Remove twilight and zen-mode plugins
    I never used them anyway.

--- neovim/dot-config/nvim/init.lua
+++ neovim/dot-config/nvim/init.lua
@@ -10,7 +10,6 @@ require("plugins.99-text-objects")
 require("plugins.99-treesitter")
 require("plugins.99-ui")
 require("plugins.99-version-control")
-require("plugins.99-writing")
 
 require("config.keymaps")
 require("config.options")
